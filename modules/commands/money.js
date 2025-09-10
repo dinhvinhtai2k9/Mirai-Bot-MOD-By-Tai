4377b3d3bbe2b35c4a60625a6bfc97629f49108d
dinhvinhtai2k9 <dinhvinhtai.2k9@gmail.com>
2025-09-10 13:37:49 +0700

    Update money.js

--- modules/commands/money.js
+++ modules/commands/money.js
@@ -1,98 +1,79 @@
 module.exports.config = {
     name: "money",
-    version: "1.1.1",
+    version: "0.0.1",
     hasPermssion: 0,
-    credits: "Quất",
-    description: "vừa setmoney vừa check money?",
-    commandCategory: "Người dùng",
-    usages: "/money [ + , - , * , / , ++ , -- , +- , +% , -% ]",
-    cooldowns: 0,
-    usePrefix: false,
-  };
-  
-  module.exports.run = async function ({ Currencies, api, event, args, Users,permssion }) {
-    const axios = require("axios")
-    const { threadID, messageID, senderID, mentions, type, messageReply } = event;
-    let targetID = senderID;
-    if (type == 'message_reply') {
-    targetID = messageReply.senderID;
-    } else if (Object.keys(mentions).length > 0) {
-    targetID = Object.keys(mentions)[0];
+    credits: "Mirai Team",//mod by ARAXY XD
+    description: "Kiểm tra số tiền của bản thân hoặc người được tag",
+    commandCategory: "Tiện ích",
+    usages: "[Tag]",
+    cooldowns: 5
+};
+
+module.exports.run = async function({ api, event, args, Currencies, Users }) {
+    const { threadID, messageID, senderID, mentions } = event;
+    const fs = require('fs');
+    const axios = require('axios');
+
+    // Hàm làm tròn số về phần nguyên gần nhất
+    function formatNumber(num) {
+        return Math.floor(num).toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
     }
-    const name = (await Users.getNameUser(targetID))
-    const i = (url) => axios.get(url, { responseType: "stream", }).then((r) => r.data);
-    const link = "https://files.catbox.moe/shxujt.gif";
-    const moment = require("moment-timezone");
-    const time = moment.tz("Asia/Ho_Chi_Minh").format('HH:mm:ss - DD/MM/YYYY');
-    const money = (await Currencies.getData(targetID)).money;
-    const mon = args[1]
-    try { switch (args[0]) {
-    case "+": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID,parseInt(mon))
-    return api.sendMessage({body:`💸 Money của ${name} được cộng thêm ${mon}$\n💸 Hiện còn ${money + parseInt(mon)}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-       
-    case "-": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID,parseInt(-mon))
-    return api.sendMessage({body:`💸 Money của ${name} bị trừ đi ${mon}$\n💸 Hiện còn ${money - mon}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-       
-    case "*": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, parseInt(money * (args[1] - 1)))
-    return api.sendMessage({body:`💸 Money của ${name} được nhân lên ${mon} lần\n💸 Hiện còn ${money * mon}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-       
-    case "/": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, parseInt(-money + (money / mon)))
-    return api.sendMessage({body:`💸 Money của ${name} bị chia đi ${args[1]} lần\n💸 Hiện còn ${money / mon}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-       
-    case "++": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, Infinity);
-    return api.sendMessage({body:`💸 Money của ${name} được thay đổi thành vô hạn\n💸 Hiện còn Infinity$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-       
-    case "--": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.decreaseMoney(targetID, parseInt(money))
-    return api.sendMessage({body:`💸 Money của ${name} bị reset\n💸 Hiện còn 0$\n⏰ ${time}`,attachment: await i(link)},event.threadID)}
-       
-    case "+-": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.decreaseMoney(targetID, parseInt(money))
-    await Currencies.increaseMoney(targetID, parseInt(mon))
-    return api.sendMessage({body:`💸 Money của ${name} được thay đổi thành ${mon}$\n💸 Money hiện tại ${mon}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-       
-    case "^": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, parseInt(-money + Math.pow(money, mon)))
-    return api.sendMessage({body:`💸 Money của ${name} được lũy thừa lên ${mon} lần\n💸 Money hiện tại ${Math.pow(money, mon)}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-  
-    case "√": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, parseInt(-money + Math.pow(money, 1/args[1])))
-    return api.sendMessage({body:`💸 Money của ${name} được căn bậc ${args[1]}\n💸 Hiện còn ${Math.pow(money, 1/args[1])}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-  
-    case "+%": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, parseInt(money / (100 / args[1])))
-    return api.sendMessage({body:`💸 Money của ${name} được cộng thêm ${args[1]}%\n💸 Hiện còn ${money + (money / (100 / args[1]))}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-  
-    case "-%": {
-    if(permssion < 2) return api.sendMessage("Bạn không đủ quyền",event.threadID)
-    await Currencies.increaseMoney(targetID, parseInt(-(money / (100 / args[1]))))
-    return api.sendMessage({body:`💸 Money của ${name} bị trừ đi ${args[1]}%\n💸 Hiện còn ${money - (money / (100 / args[1]))}$\n⏰ ${time}`,attachment:await i(link)},event.threadID)}
-        
-    case "pay": {
-    const money2 = (await Currencies.getData(event.senderID)).money;
-    var bet = args[1] === 'all' ? money2 : args[1]
-    if (money < 1) return api.sendMessage({body:"Bạn có ít hơn 1$ hoặc bạn số tiền chuyển lớn hơn số dư của bạn",attachment: await i(link)},event.threadID)
-    await Currencies.increaseMoney(event.senderID, parseInt(-bet))
-    await Currencies.increaseMoney(targetID, parseInt(bet))
-   return api.sendMessage(`Đã chuyển cho ${name} ${bet}$`,event.threadID)}
-  } 
-        } catch(e) {console.log(e)}
-    if (money === Infinity) return api.sendMessage(`${name} có vô hạn tiền`,event.threadID)
-    if (money === null) return api.sendMessage(`${name} có 0$`,event.threadID)
-    if (!args[0] || !args[1]) return api.sendMessage(`${name} có ${money}$`,event.threadID)
-  }
+
+    if(!fs.existsSync(__dirname+'/cache/SplineSans-Medium.ttf')) { 
+        let getfont = (await axios.get(`https://drive.google.com/u/0/uc?id=102B8O3_0vTn_zla13wzSzMa-vdTZOCmp&export=download`, { responseType: "arraybuffer" })).data;
+        fs.writeFileSync(__dirname+"/cache/SplineSans-Medium.ttf", Buffer.from(getfont, "utf-8"));
+    };
+    if(!fs.existsSync(__dirname+'/cache/SplineSans.ttf')) { 
+        let getfont2 = (await axios.get(`https://drive.google.com/u/0/uc?id=1--V7DANKLsUx57zg8nLD4b5aiPfHcmwD&export=download`, { responseType: "arraybuffer" })).data;
+        fs.writeFileSync(__dirname+"/cache/SplineSans.ttf", Buffer.from(getfont2, "utf-8"));
+    };
+
+    let name, money;
+
+    if (event.type == "message_reply") {
+        const uid = event.messageReply.senderID;
+        name = (await Users.getData(uid)).name;
+        money = (await Currencies.getData(uid)).money || 0;
+    } else if (Object.keys(event.mentions).length == 1) {
+        const mention = Object.keys(mentions)[0];
+        name = (await Users.getData(mention)).name;
+        money = (await Currencies.getData(mention)).money || 0;
+    } else {
+        name = (await Users.getData(senderID)).name;
+        money = (await Currencies.getData(senderID)).money || 0;
+    }
+
+    const argss = formatNumber(money);
+
+    const { loadImage, createCanvas } = require("canvas");
+    const path = __dirname + "/cache/atmaraxy.png";
+    const bg = (await axios.get(`https://i.ibb.co/NZD1Zzz/image.png`, {responseType: "arraybuffer" })).data;
+    fs.writeFileSync(path, Buffer.from(bg, "utf-8"));
+    const bgBase = await loadImage(path);
+    const canvas = createCanvas(bgBase.width, bgBase.height);
+    const ctx = canvas.getContext("2d");
+    const Canvas = global.nodemodule["canvas"];
+    ctx.drawImage(bgBase, 0, 0, canvas.width, canvas.height);
+    Canvas.registerFont(__dirname+`/cache/SplineSans-Medium.ttf`, {
+        family: "SplineSans-Medium"
+    });
+    Canvas.registerFont(__dirname+`/cache/SplineSans.ttf`, {
+        family: "SplineSans"
+    });
+    ctx.font = "50px SplineSans-Medium";
+    ctx.fillStyle = "#000000";
+    ctx.textAlign = "center";
+    ctx.fillText('' + argss + 'đ', 530, 359);
+    const imageBuffer = canvas.toBuffer();
+    fs.writeFileSync(path, imageBuffer);
+
+    const msg = {
+        body: `Số tiền của bạn ${name} đây\nSố tiền của bạn đang có là ${argss}`,
+        attachment: fs.createReadStream(path)
+    };
+
+    return api.sendMessage(msg, threadID, async (error, info) => {
+        fs.unlinkSync(path);
+        messageID;
+    });
+};
